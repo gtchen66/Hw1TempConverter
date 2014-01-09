ed09d0b187f663a8a6ec51edf984674a3eb9798a
gtchen66 <gtchen66@yahoo.com>
2014-01-09 01:55:36 -0800

    Initial Commit

---
+++ Hw1TempConverterTests/Hw1TempConverterTests.m
@@ -0,0 +1,34 @@
+//
+//  Hw1TempConverterTests.m
+//  Hw1TempConverterTests
+//
+//  Created by George Chen on 1/9/14.
+//  Copyright (c) 2014 George Chen. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Hw1TempConverterTests : XCTestCase
+
+@end
+
+@implementation Hw1TempConverterTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
